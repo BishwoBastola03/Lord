05059bba6d39f7d40ceef5429e0bc06d6c2ae0dd
BishwoBastola03 <161615824+BishwoBastola03@users.noreply.github.com>
2024-03-24 20:43:28 +0000

    Initial commit

---
+++ animation.js
@@ -0,0 +1,6 @@
+const animation = document.getElementById('animation');
+const enterButton = document.getElementById('enter-button');
+
+enterButton.addEventListener('click', () => {
+  // Start the 3D animation here
+});
